058189f6a5f39136ca57c75c6384c69dab5422ae
naoki <0512.naoki.job@gmail.com>
2025-06-27 22:06:09 +0900

    commit

---
+++ valid-parentheses.cpp
@@ -0,0 +1,24 @@
+#include <stack>
+#include <string>
+using namespace std;
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char c:s){
+            if(c=='('||c=='['||c=='{'){
+                st.push(c);
+            }
+            else{
+                if(st.size()==0){return false;}
+                char i=st.top();
+                st.pop();
+                if((c == ')' and i == '(') or  (c == ']' and i== '[') or (c == '}' and i == '{')) continue;
+                else{return false;}
+            }
+        }
+        return st.empty();
+    }
+};
