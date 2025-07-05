62d71fd937be8b5661fc6654bc85c5a8cc2ee4c3
naoki <0512.naoki.job@gmail.com>
2025-07-05 15:50:20 +0900

    コミット

---
+++ #67. Add Binary.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i = a.size() - 1;  
+        int j = b.size() - 1; 
+        int carry = 0; // 繰り上がりが入る
+        std::string result = "";
+
+        while(i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+
+            if(i >= 0) {
+                sum += a[i] - '0';
+                i--;
+            }
+
+            if(j >= 0) {
+                sum += b[j] - '0';
+                j--;
+            }
+
+            result += (sum % 2) + '0';
+            carry = sum / 2;
+        }
+
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
